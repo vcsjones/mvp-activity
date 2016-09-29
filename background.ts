898cc9d97f51e729bea83027c506a574d673415a
Kevin Jones <kj@upside.com>
2016-09-29 13:32:57 -0400

    Removed debugging code.

--- background.ts
+++ background.ts
@@ -3,11 +3,9 @@ import {QueryResponse} from './model'
 class TabQueryListener {
     initialize() {
         chrome.runtime.onMessage.addListener((request : string, sender, response) => {
-            console.log(request);
             if (request !== 'taburl') {
                 return;
             }
-            console.log(sender);
             chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
                 if (tabs.length !== 1) {
                     return;
