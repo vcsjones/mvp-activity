f3023e7ae6e408c5d3009340a878b246c8fca727
Kevin Jones <kj@upside.com>
2016-10-18 13:17:43 -0400

    Organize files into src directory.
    Supporting build files and typings should not be along side the actual
    application source.

--- build.sh
+++ build.sh
@@ -2,11 +2,13 @@
 
 npm install
 mkdir -p ./out
-tsc --outDir ./out
-cp *.html ./out
-cp *.css ./out
-cp manifest.json ./out/manifest.json
-cp -r ./images/ ./out/images/
+mkdir -p ./out/css
+mkdir -p ./out/script
+tsc --outDir ./out/script
+cp src/*.html ./out
+cp src/css/*.css ./out/css
+cp src/manifest.json ./out/manifest.json
+cp -r ./src/images/ ./out/images/
 pushd ./out
 zip -r -X ../out.zip *
 popd 
